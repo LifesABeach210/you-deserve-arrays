7c07991cf65f41db44dd0c9b52d36f298bcde07b
Daniel Rothwell <danielrothwell@Daniels-MacBook-Pro.local>
2022-02-14 17:50:38 -0500

    F

---
+++ getFirstItem.js
@@ -0,0 +1,25 @@
+const prompt = require("prompt-sync")();
+
+let userArray = JSON.parse(prompt("enter your array:"));
+let firstItem = userArray[0];
+let thirdItem = userArray[2];
+let arrayLength = userArray.length[10];
+let lastItem = userArray[arrayLength-1];
+let longList = userArray.length[10];
+var btrue = Boolean('true')
+var bfalse = Boolean('false')
+//console.log("the third item in the array is"+thirdItem);
+
+//consol.log("your array is this" +typeof(a
+
+//if (userArray = thirdItem){
+//console.log("the third index in thhis array is"+thirdItem);
+
+//}else {
+//console.log("the last index in the array is"+lastItem);
+//}
+//if (longList!==arrayLength){console.log(bfalse);
+if (userArray[1]!== Number){console.log(btrue)
+
+
+}else {console.log("error");}
